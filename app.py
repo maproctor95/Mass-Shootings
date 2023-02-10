d63dbe13eb84e747c0f6cb51b592d999ba14ca5f
lafnggl <114120881+lafnggl@users.noreply.github.com>
2023-02-09 20:26:51 -0500

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,5 @@ app = Flask(__name__)
 @app.route('/')
 def mass_shootings():
     df=pd.read_csv('data/Mass_Shooting_SQL_Export_data.csv')
-    for shooter_sex in df:
-        df_dict = {}
-        df_dict['shooter_sex'] = shooter_sex
-    return jsonify(df_dict)
+    results = df.to_dict()
+    return jsonify(results)
