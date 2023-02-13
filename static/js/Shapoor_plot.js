30d944b4fbeedd8afe5151af22e718694fd861b5
lafnggl <114120881+lafnggl@users.noreply.github.com>
2023-02-13 18:48:14 -0500

    htlm/readme update

--- static/js/Shapoor_plot.js
+++ static/js/Shapoor_plot.js
@@ -1,24 +1,15 @@
 var options = {
     series: [93, 119, 95],
-    chart: {
-    width: 380,
+title: {
+    text: "History of Mental Illness"
+      },
+chart: {
     type: 'pie',
   },
   labels: ['No', 'Unknown', 'Yes'],
-  responsive: [{
-    breakpoint: 480,
-    options: {
-      chart: {
-        width: 200
-      },
-      title: {
-        text: "History of Mental Illness - General"
-      },
-      legend: {
-        position: 'bottom'
+  legend: {
+    position: 'bottom'
       }
-    }
-  }]
   };
 
   var chart = new ApexCharts(document.querySelector("#chart"), options);
